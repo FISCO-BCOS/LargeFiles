5cb90c7a37d3cf63361143a65ecdf8ba9ef23e24
bxq2011hust <bxq2011hust@qq.com>
2019-05-20 16:20:40 +0800

    modify get_account pem to remove EC

--- tools/get_account.sh
+++ tools/get_account.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a83fd0ea25a4e1ceb489108ead84377194964cf1cadaaefde3188e281232d2e2
-size 491264
+oid sha256:291cfb628c5be2d261b064c48ba30d6f60919cda9b1f69bec4265b1b4abe6e5b
+size 491312
