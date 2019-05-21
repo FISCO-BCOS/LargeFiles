d9707f6fe3c83ecac58a293b145cbf3109212ffd
bxq2011hust <bxq2011hust@qq.com>
2019-05-21 17:53:20 +0800

    update get_account.sh

--- tools/get_account.sh
+++ tools/get_account.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:291cfb628c5be2d261b064c48ba30d6f60919cda9b1f69bec4265b1b4abe6e5b
-size 491312
+oid sha256:10fb4ed0e03d88eb4f21c9fc250d02a7a032726d1cb9371c069ec067b9a850a7
+size 491352
