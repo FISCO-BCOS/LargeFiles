9948094f8fba8bf256274f3d411dd881aad3542c
bxq2011hust <bxq2011hust@qq.com>
2019-08-30 12:52:35 +0800

    modify get_account.sh to check p12 file suffix

--- tools/get_account.sh
+++ tools/get_account.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53d8028e29d2e110e88c39aa49a2c07fc650e552c684b7e33fc65c4680aab7e5
-size 491828
+oid sha256:816e479df410e56ac98e27497bdee25208371bc8746add5431fd7a934c60e554
+size 492193
